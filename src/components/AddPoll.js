098ad83d014f762b3670bb25099eab061aa73d5c
Armand Biglary <armand.biglary@gmail.com>
2019-02-28 12:34:10 +0100

    armand // front : changed AddPoll.js

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -2,20 +2,93 @@ import React, { Component } from "react";
 import "./AddPoll.css";
 
 class AddPoll extends Component {
-  // Component's constructor, remove if not necessary
   constructor(props) {
     super(props);
-    // Component's state, remove if not necessary
-    this.state = {};
+    this.state = {
+      shortText: "",
+      longText: "",
+      image: "",
+      isSubmitSuccessful: false
+    };
   }
-  // Component's method
-  componentMethod() {}
 
-  // Component structure and display logic go her
+  genericOnChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    postPoll(this.state).then(() =>
+      this.setState({ isSubmitSuccessful: true })
+    );
+  }
+
   render() {
-    return (
+    return this.state.isSubmitSuccessful ? (
+      <Redirect to="/browse-polls" />
+    ) : (
       <section className="AddPoll">
-        <h2>AddPoll</h2>
+        <h2>Create a new Poll</h2>
+
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <label>
+            Title:{" "}
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.shortText}
+              name="phoneModel"
+              type="text"
+              placeholder="iPhone Xs"
+            />
+          </label>
+
+          <label>
+            Brand:{" "}
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.brand}
+              name="brand"
+              type="text"
+              placeholder="Apple"
+            />
+          </label>
+
+          <label>
+            Price:{" "}
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.price}
+              name="price"
+              type="number"
+              placeholder="1155"
+            />
+          </label>
+
+          <label>
+            Image URL:{" "}
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.image}
+              name="image"
+              type="url"
+              placeholder="http://example.com"
+            />
+          </label>
+
+          <label>
+            Specs:{" "}
+            <input
+              onChange={event => this.genericOnChange(event)}
+              value={this.state.specs}
+              name="specs"
+              type="text"
+              placeholder="great camera, 8-hour battery"
+            />
+          </label>
+
+          <button>Submit This Phone</button>
+        </form>
       </section>
     );
   }
